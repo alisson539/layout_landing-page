1ed00f6b1cd646790a746fe91b3a4b7adef9d7f1
Alisson <alissonramos539@gmail.com>
2025-11-06 14:54:06 -0300

    Add js and image links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const closeIcon = document.querySelector(".close-icon");
+  const menu = document.querySelector(".page__menu");
+
+  if (burger && closeIcon && menu) {
+    burger.addEventListener("click", (e) => {
+      e.preventDefault(); // prevent jump to #menu
+      menu.classList.add("active");
+    });
+
+    closeIcon.addEventListener("click", (e) => {
+      e.preventDefault(); // prevent jump to #menu
+      menu.classList.remove("active");
+    });
+  }
+});
+
+
+document.querySelectorAll(".menu__item a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
